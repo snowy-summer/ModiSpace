c4af12ca74ba78ccfbb35821cc492a4a7c230149
snowy-summer <118453865+snowy-summer@users.noreply.github.com>
2024-10-24 22:22:32 +0900

    Initial Commit

---
+++ ModiSpace/ModiSpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModiSpaceApp.swift
+//  ModiSpace
+//
+//  Created by 최승범 on 10/24/24.
+//
+
+import SwiftUI
+
+@main
+struct ModiSpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
