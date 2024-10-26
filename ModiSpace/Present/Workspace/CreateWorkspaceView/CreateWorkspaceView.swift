7bd4ee29c8e034b3ba46656c630e239e48acb232
snowy-summer <118453865+snowy-summer@users.noreply.github.com>
2024-10-27 00:32:48 +0900

    feat: CreateWorkspaceView 생성, MVI 적용

---
+++ ModiSpace/Present/Workspace/CreateWorkspaceView/CreateWorkspaceView.swift
@@ -0,0 +1,56 @@
+//
+//  CreateWorkspaceView.swift
+//  ModiSpace
+//
+//  Created by 최승범 on 10/26/24.
+//
+
+import SwiftUI
+import Combine
+
+struct CreateWorkspaceView: View {
+    
+    @StateObject private var model: CreateWorkSpaceModel
+    private var intent: CreateWorkspaceIntent
+    
+    var onCreate: () -> Void = {}
+    
+    init(onCreate: @escaping () -> Void = {}) {
+        _model = StateObject(wrappedValue: CreateWorkSpaceModel())
+        self.intent = CreateWorkspaceIntent()
+        self.onCreate = onCreate
+    }
+    
+    var body: some View {
+        VStack(spacing: 24) {
+            
+            InputFieldCell(text: $model.workspaceName,
+                           title: "워크스페이스 이름",
+                           placeholder: "워크스페이스 이름을 입력하세요 (필수)")
+            InputFieldCell(text: $model.workspaceDescription,
+                           title: "워크스페이스 설명",
+                           placeholder: "워크스페이스를 설명하세요 (옵션)")
+            
+            Spacer()
+            
+            BasicLargeButtonCell(title: "완료", isEnabled: model.isCreateAbled) {
+                print(model.workspaceName)
+                intent.createWorkspace() 
+            }
+            .padding()
+            .disabled(!model.isCreateAbled)
+        }
+        .padding(.top, 30)
+        .background(Color.gray.opacity(0.2))
+        .onAppear {
+            intent.setModel(model)
+        }
+        .onTapGesture {
+            endTextEditing()
+        }
+    }
+}
+
+#Preview {
+    CreateWorkspaceView()
+}
---
+++ ModiSpace/Present/Workspace/CreateWorkspaceView/Model/Implementaion/CreateWorkspaceModel.swift
@@ -0,0 +1,30 @@
+//
+//  CreateWorkspaceModel.swift
+//  ModiSpace
+//
+//  Created by 최승범 on 10/26/24.
+//
+
+import Foundation
+
+final class CreateWorkSpaceModel: ObservableObject, CreateWorkspaceModelStateProtocol {
+    
+    @Published var workspaceName = ""
+    @Published var workspaceDescription = ""
+    var isCreateAbled: Bool {
+        !workspaceName.isEmpty
+    }
+    
+}
+
+extension CreateWorkSpaceModel: CreateWorkspaceModelActionsProtocol {
+    
+    func updateWorkspaceName(_ name: String) {
+        workspaceName = name
+    }
+    
+    func updateWorkspaceDescription(_ description: String) {
+        workspaceDescription = description
+    }
+    
+}
