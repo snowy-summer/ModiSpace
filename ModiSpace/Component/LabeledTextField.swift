382a07138024ef763778933e492a8f538412e247
JunYeong-J <eeot2827@gmail.com>
2024-10-26 22:02:17 +0900

    feat: 공통으로 사용가능하도록 textField 생성

---
+++ ModiSpace/Component/LabeledTextField.swift
@@ -0,0 +1,18 @@
+//
+//  LabeledTextField.swift
+//  ModiSpace
+//
+//  Created by 전준영 on 10/26/24.
+//
+
+import SwiftUI
+
+struct LabeledTextField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LabeledTextField()
+}
