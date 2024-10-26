2fa33d908023c6b9d9037fb9c1d010fd07ec390b
JunYeong-J <eeot2827@gmail.com>
2024-10-26 22:02:56 +0900

    feat: 공통으로 사용가능하도록 textField 생성

--- ModiSpace/Component/LabeledTextField.swift
+++ ModiSpace/Component/LabeledTextField.swift
@@ -8,11 +8,53 @@
 import SwiftUI
 
 struct LabeledTextField: View {
+    
+    let labelText: String? //라벨 없을수 있으니 옵셔널
+    let placeholder: String
+    var keyboardType: UIKeyboardType = .default //키보드 타입 선정 가능(기본값 미리 설정)
+    
+    @Binding var text: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack(alignment: .leading, spacing: 6) {
+            
+            if let labelText = labelText {
+                Text(labelText)
+                    .customFont(.title1)
+                    .foregroundStyle(.black)
+            }
+            
+            TextField(placeholder, text: $text)
+                .padding()
+                .background(.white)
+                .cornerRadius(8)
+                .customFont(.title2)
+                .keyboardType(keyboardType)
+                .accentColor(.black)
+            
+        }
+        
     }
+    
 }
 
-#Preview {
-    LabeledTextField()
+struct LabeledTextField_Previews: PreviewProvider {
+    @State static var text: String = ""
+    
+    static var previews: some View {
+        VStack(spacing: 20) {
+            LabeledTextField(labelText: "연락처",
+                             placeholder: "전화번호를 입력하세요",
+                             text: $text)
+            
+            //라벨 없을때 사용, 키보드 설정
+            LabeledTextField(labelText: nil,
+                             placeholder: "전화번호를 입력하세요",
+                             keyboardType: .numberPad, text: $text)
+            
+        }
+        .padding()
+        .background(.backgroundPrimary)
+    }
 }
