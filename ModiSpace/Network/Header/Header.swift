3ff4ccb63ec91ff689d71389747a1f4d9e7f5eaf
snowy-summer <118453865+snowy-summer@users.noreply.github.com>
2024-10-26 13:25:34 +0900

    feat: Header enum 추가

--- ModiSpace/Network/Header/Header.swift
+++ ModiSpace/Network/Header/Header.swift
@@ -6,3 +6,49 @@
 //
 
 import Foundation
+
+enum Header {
+    
+    case authorization
+    case sesacKey
+    case refreshToken
+    case contentTypeJson
+    case contentTypeMulti
+    
+    var key: String {
+        switch self {
+        case .authorization:
+            return "Authorization"
+            
+        case .sesacKey:
+            return "SesacKey"
+            
+        case .refreshToken:
+            return "RefreshToken"
+            
+        case .contentTypeJson, .contentTypeMulti:
+            return "Content-Type"
+        }
+    }
+    
+    var value: String {
+        switch self {
+        case .authorization:
+            return "----"
+            
+        case .sesacKey:
+            guard let key = Bundle.main.object(forInfoDictionaryKey: "APIKey") as? String else { return "실패" }
+            return key
+            
+        case .refreshToken:
+            return "----"
+            
+        case .contentTypeJson:
+            return "application/json"
+            
+        case .contentTypeMulti:
+            return "multipart/form-data"
+        }
+        
+    }
+}
