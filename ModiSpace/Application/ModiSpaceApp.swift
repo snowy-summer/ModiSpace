1b77fbc427021df6c4571f7ed931956192740c77
JunYeong-J <eeot2827@gmail.com>
2024-10-27 20:29:29 +0900

    feat: 스플래시 화면 추가, 스플래시 화면 1초로 설정, 스플래시 화면에 사용할 이미지 저장

--- ModiSpace/Application/ModiSpaceApp.swift
+++ ModiSpace/Application/ModiSpaceApp.swift
@@ -8,10 +8,41 @@
 import SwiftUI
 
 @main
-struct ModiSpaceApp: App {
+struct CampairApp: App {
+    
+    @State var isSplashView = true
+    
     var body: some Scene {
+        
         WindowGroup {
-            ContentView()
+            
+            if isSplashView {
+                LaunchScreenView()
+                    .ignoresSafeArea()
+                    .onAppear {
+                        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1) { // 1초로 설정
+                            isSplashView = false
+                        }
+                    }
+            } else {
+                ContentView()
+            }
+            
         }
+        
     }
+}
+
+
+struct LaunchScreenView: UIViewControllerRepresentable {
+    
+    func makeUIViewController(context: Context) -> some UIViewController {
+        let controller = UIStoryboard(name: "Launch Screen", bundle: nil).instantiateInitialViewController()!
+        return controller
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        
+    }
+    
 }
