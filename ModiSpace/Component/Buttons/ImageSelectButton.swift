82e1caeb3066da2dd215335f4d17c6f57c086eb4
snowy-summer <118453865+snowy-summer@users.noreply.github.com>
2024-10-31 09:49:07 +0900

    refactor: 팀토의 결과 반영 -> ImageSelectButton 수정

--- ModiSpace/Component/Buttons/ImageSelectButton.swift
+++ ModiSpace/Component/Buttons/ImageSelectButton.swift
@@ -9,37 +9,41 @@ import SwiftUI
 
 struct ImageSelectButton: View {
     
-    var tapGesture: () -> Void
+    var action: () -> Void
     
     var body: some View {
-        Image(uiImage: .temp)
-            .resizable()
-            .frame(width: 100, height: 100)
-            .clipShape(RoundedRectangle(cornerRadius: 12))
-            .overlay(
-                ZStack {
-                    
-                    Circle()
-                        .fill(.white)
-                        .frame(width: 40, height: 40)
-                        .offset(x: 10, y: 10)
-                    
-                    Image(systemName: "camera.circle.fill")
-                        .resizable()
-                        .frame(width: 32, height: 32)
-                        .foregroundColor(.main)
-                        .background(Color.white)
-                        .clipShape(Circle())
-                        .offset(x: 10, y: 10)
-                },
-                alignment: .bottomTrailing
-            )
-            .onTapGesture {
-                tapGesture()
-            }
+        
+        Button(action: action) {
+            Image(uiImage: .temp)
+                .resizable()
+                .frame(width: 100, height: 100)
+                .clipShape(RoundedRectangle(cornerRadius: 12))
+                .overlay(
+                    ZStack {
+                        Circle()
+                            .fill(.white)
+                            .frame(width: 40,
+                                   height: 40)
+                            .offset(x: 10,
+                                    y: 10)
+                        
+                        Image(systemName: "camera.circle.fill")
+                            .resizable()
+                            .frame(width: 32,
+                                   height: 32)
+                            .foregroundColor(.main)
+                            .background(.white)
+                            .clipShape(Circle())
+                            .offset(x: 10,
+                                    y: 10)
+                    },
+                    alignment: .bottomTrailing
+                )
+        }
     }
+    
 }
 
 #Preview {
-    ImageSelectButton(tapGesture: {})
+    ImageSelectButton(action: {})
 }
