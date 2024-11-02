4df10eed00cc0492153af177167e8c87c977b317
Acasiax <117105267+Acasiax@users.noreply.github.com>
2024-11-02 13:14:50 +0900

    feat: ChatIntent 채팅 인텐트 생성

---
+++ ModiSpace/Present/ChattingView/Intent/ChatIntent.swift
@@ -0,0 +1,14 @@
+//
+//  ChatIntent.swift
+//  ModiSpace
+//
+//  Created by 이윤지 on 11/2/24.
+//
+
+import SwiftUI
+
+enum ChatIntent {
+    case sendMessage(String, [UIImage])
+    case removeImage(Int)
+    case showImagePicker(Bool)
+}
