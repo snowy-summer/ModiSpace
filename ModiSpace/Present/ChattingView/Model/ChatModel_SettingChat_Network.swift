552577927692b6d2061f879eb51a50522bda1048
Acasiax <117105267+Acasiax@users.noreply.github.com>
2024-11-11 21:09:02 +0900

    feat: 채널 삭제 기능 로직 구현

---
+++ ModiSpace/Present/ChattingView/Model/ChatModel_SettingChat_Network.swift
@@ -0,0 +1,36 @@
+//
+//  ChatModel_SettingChat_Network.swift
+//  ModiSpace
+//
+//  Created by 이윤지 on 11/11/24.
+//
+
+import SwiftUI
+import Combine
+
+extension ChatModel {
+    
+    func deleteChannel() {
+        networkManager.getDataWithPublisher(from: ChannelRouter.deleteSpecificChannel(workspaceID: WorkspaceIDManager.shared.workspaceID ?? "", channelID: channel.channelID))
+            .receive(on: DispatchQueue.main)
+            .sink { completion in
+                switch completion {
+                case .finished:
+                    print("채널 삭제 성공")
+                    self.isChannelDeleted = true
+                case .failure(let error):
+                    if let error = error as? NetworkError {
+                        print(error.description)
+                    }
+                    if let error = error as? APIError, error == .refreshTokenExpired {
+                        print("리프레시 토큰 만료")
+                    }
+                    print(error.localizedDescription)
+                }
+            } receiveValue: { [weak self] data in
+                
+            }
+            .store(in: &cancelable)
+    }
+    
+}
