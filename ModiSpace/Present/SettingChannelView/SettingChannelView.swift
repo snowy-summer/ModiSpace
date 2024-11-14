3947e384b6cf628964120e27ac8e73e5c14dfb37
Acasiax <117105267+Acasiax@users.noreply.github.com>
2024-11-14 16:28:32 +0900

    feat: 채널 설정뷰에 스크롤뷰 추가

--- ModiSpace/Present/SettingChannelView/SettingChannelView.swift
+++ ModiSpace/Present/SettingChannelView/SettingChannelView.swift
@@ -12,48 +12,53 @@ struct SettingChannelView: View {
     @StateObject var model: ChatModel
     
     var body: some View {
-        VStack {
-            SettingChannelHeaderView(model: model)
-            
-            MemberList()
-            
-            SettingChannelButton(title: "채널 편집") {
-                model.apply(.showEditChannelView)
-            }
-            
-            SettingChannelButton(title: "채널에서 나가기") { }
-            
-            SettingChannelButton(title: "채널 관리자 변경") {
-                //model.apply(.showChangeManagerView)
-            }
-            
-            SettingChannelButton(title: "채널 삭제") {
-                model.isShowDeleteAlertView = true
-            }
-        }
-        .overlay(
-            Group {
-                if model.isShowDeleteAlertView {
-                    AlertView(
-                        title: "채널 삭제",
-                        message: "정말 이 채널을 삭제하시겠습니까? 삭제 시 모든 데이터가 사라지며 복구할 수 없습니다.",
-                        primaryButtonText: "취소",
-                        secondaryButtonText: "삭제",
-                        onPrimaryButtonTap: { model.apply(.dontShowDeleteAlert) },
-                        onSecondaryButtonTap: { model.apply(.deleteChannel) }
-                    )
+        ScrollView {
+            VStack {
+                SettingChannelHeaderView(model: model)
+                
+                MemberList()
+                
+                SettingChannelButton(title: "채널 편집") {
+                    model.apply(.showEditChannelView)
+                }
+                
+                SettingChannelButton(title: "채널에서 나가기") { }
+                
+                SettingChannelButton(title: "채널 관리자 변경") {
+                    //model.apply(.showChangeManagerView)
+                }
+                
+                SettingChannelButton(title: "채널 삭제") {
+                    model.isShowDeleteAlertView = true
                 }
             }
-        )
-        .sheet(isPresented: $model.isShowingEditChannelView) {
-                   EditChannelView(model: model)
-                .presentationDragIndicator(.visible)
-               }
-        .onChange(of: model.isChannelDeleted) { isDeleted in
-            if isDeleted {
-                switchToWorkspaceView()
+            .overlay(
+                Group {
+                    if model.isShowDeleteAlertView {
+                        AlertView(
+                            title: "채널 삭제",
+                            message: "정말 이 채널을 삭제하시겠습니까? 삭제 시 모든 데이터가 사라지며 복구할 수 없습니다.",
+                            primaryButtonText: "취소",
+                            secondaryButtonText: "삭제",
+                            onPrimaryButtonTap: { model.apply(.dontShowDeleteAlert) },
+                            onSecondaryButtonTap: { model.apply(.deleteChannel) }
+                        )
+                    }
+                }
+            )
+            .sheet(isPresented: $model.isShowingEditChannelView) {
+                EditChannelView(model: model)
+                    .presentationDragIndicator(.visible)
             }
+            .onChange(of: model.isChannelDeleted) { isDeleted in
+                if isDeleted {
+                    switchToWorkspaceView()
+                }
+            }
+            
         }
+        
+        
     }
     
 }
